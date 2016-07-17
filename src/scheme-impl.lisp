3ab691fa58d2f76dd8bd170bf634d85aa2f740ef
iyahoo <s1200191@gmail.com>
2016-07-18 02:18:49 +0900

    [Add] 第2章終わりまで

--- src/scheme-impl.lisp
+++ src/scheme-impl.lisp
@@ -8,9 +8,11 @@
 (annot:enable-annot-syntax)
 
 ;; 参考: http://tatsu-zine.com/samples/free/scheme-in-ruby.pdf
-;; これを CL で実装する。構文を [:+, x, y] にしている理由が Ruby で簡単に読み込めるためということで、今回は (:+ x y) の構文に置きかえた
+;; これを CL で実装する。構文を [:+, x, y] にしている理由が Ruby で簡単に読み込めるためということで、今回は (:+ :x :y) の構文に置きかえた
+;; 関数名、変数名を共にキーワードとする
 
-;; 開発時用
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 開発用
 ;; M-x slime-restart-inferior-lisp
 ;; ↓
 (defun in-env ()
@@ -18,9 +20,8 @@
   (in-package :scheme-impl)
   (asdf:test-system :scheme-impl))
 
-(defun run-test ()
-  (asdf:test-system :scheme-impl))
-;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; global variable
 
 (defparameter *primitive-fun-env*
   #H(:+ '(:prim +)
@@ -28,35 +29,139 @@
      :* '(:prim *)))
 
 @export
-(defun lookup-primitive-fun (exp)
-  (aif (getf *primitive-fun-env* exp)
+(defparameter *global-env* *primitive-fun-env*)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; lookup
+
+;; @export
+;; (defun lookup-primitive-fun (exp)
+;;   (aif (getf *primitive-fun-env* exp)
+;;        it
+;;        (error "~S is undefined function or don't expected type" exp)))
+
+@export
+(defun lookup-var (var env)
+  (aif (getf env var)
        it
-       (error "~S is undefined function or don't expected type" exp)))
+       (error "couldn't find value to variables:~S" var)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; predicate
 
 (defun immediate-val-p (exp)
   (numberp exp))
+
+@export
+(defun letp (exp)
+  (and
+   (eq :let (first exp))
+   (listp (second exp))
+   (third exp)))
+
+(defun lambdap (exp)
+  (and
+   (eq :lambda (first exp))
+   (listp (second exp))
+   (third exp)))
+
+@export
+(defun special-form-p (exp)
+  (or (letp exp) (lambdap exp)))
+
+(defun primitive-fun-p (exp)
+  (eq (first exp) :prim))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; env
+
+@export
+
+(defun extend-env (parameters args env)
+  (let ((new-env #H()))
+    (loop for value being the hash-values of env
+            using (hash-key key)
+          :do (setf (getf new-env key) value))
+    (loop :for x :in parameters
+          :for y :in args
+          :do (setf (getf new-env x) y))
+    new-env))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; closure
+
+(defun make-closure (exp env)
+  (let ((parameters (second exp))
+        (body (third exp)))
+    `(:closure ,parameters ,body ,env)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; apply
 
 @export
 (defun apply-primitive-fun (fun args)
   (let ((fun-val (second fun)))
     (apply fun-val args)))
 
+@export
+(defun apply-lambda (closure args)
+  (destructuring-bind (parameters body env) (closure-to-parameters-body-env closure)
+    (let ((new-env (extend-env parameters args env)))
+      (_eval body new-env))))
+
+@export
 (defun si/apply (fun args)
-  (apply-primitive-fun fun args))
+  (cond ((primitive-fun-p fun) (apply-primitive-fun fun args))
+        (t (apply-lambda fun args))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; parse
+
+@export
+(defun closure-to-parameters-body-env (closure)
+  `(,(second closure) ,(third closure) ,(fourth closure)))
+
+@export
+(defun let-to-parameters-args-body (exp)
+  `(,(map #'first (second exp))
+    ,(map #'second (second exp))
+    ,(third exp)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; eval
 
 @export
 (defun si/eval (string-exp)
   (assert (stringp string-exp))
-  (_eval (read-from-string string-exp)))
+  (_eval (read-from-string string-exp) *global-env*))
 
-(defun _eval-list (exp)
-  (map #'_eval exp))
+@export
+(defun eval-let (exp env)
+  (destructuring-bind (parameters args body) (let-to-parameters-args-body exp)
+    (let ((new-exp (append `((:lambda ,parameters ,body)) args)))
+      (_eval new-exp env))))
 
-(defun _eval (exp)
+@export
+(defun eval-lambda (exp env)
+  (make-closure exp env))
+
+(defun eval-special-form (exp env)
+  (cond ((lambdap exp) (eval-lambda exp env))
+        ((letp exp) (eval-let exp env))
+        (t (error "~S is unmatched as special form." exp))))
+
+@export
+(defun eval-list (exp env)
+  (map ^(_eval %1 env) exp))
+
+@export
+(defun _eval (exp env)
   (if (listp exp)
-      (let ((fun (_eval (first exp)))
-            (args (_eval-list (rest exp))))
-        (si/apply fun args))
+      (if (special-form-p exp)
+          (eval-special-form exp env)
+          (let ((fun (_eval (first exp) env))
+                (args (eval-list (rest exp) env)))
+            (si/apply fun args)))
       (if (immediate-val-p exp)
           exp
-          (lookup-primitive-fun exp))))
+          (lookup-var exp env))))
--- t/scheme-impl.lisp
+++ t/scheme-impl.lisp
@@ -11,15 +11,80 @@
 
 (plan nil)
 
-(subtest "lookup-primitive-fun & apply-primitive-fun"
-  (is (apply-primitive-fun (lookup-primitive-fun :+) '(1 2 3)) 6)
-  (is (apply-primitive-fun (lookup-primitive-fun :*) '(2 2 2 2 2)) 32)
-  (is-error (apply-primitive-fun (lookup-primitive-fun :car) '(1 2 3)) 'simple-error))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; global variable
+
+(defparameter *env* *global-env*)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; lookup
+
+(subtest "lookup-var"
+  (is (lookup-var :y #H(:x 1 :y 2)) 2))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; predicate
+
+(subtest "letp"
+  (ok (letp '(:let ((:x 1) (:y 2)) :x)))
+  (ok (not (letp '(:let ())))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; env
+
+(subtest "extend-env"
+  (is (extend-env '(:x :z) '(1 2) #H(:y 3))
+      #H(:z 2 :x 1 :y 3)
+      :test #'equalp)
+  (is (extend-env '(:x :y) '(2 4) #H(:z 4))
+      #H(:x 2 :y 4 :z 4)
+      :test #'equalp))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; apply
+
+(subtest "si/apply"
+  (is (si/apply '(:prim +) '(1 2 3)) 6))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; parse
+
+(subtest "closure-to-parameters-body-env"
+  (is (closure-to-parameters-body-env '(:closure (:x :y) (:+ :x :y :z) #H(:z 4)))
+      '((:x :y) (:+ :x :y :z) #H(:z 4))))
+
+(subtest "let-to-parameters-args-body"
+  (is (let-to-parameters-args-body '(:let ((:x 1) (:y 2)) (:+ :x :y)))
+      '((:x :y) (1 2) (:+ :x :y))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; eval
+
+(subtest "eval-lambda" 
+  (is (eval-lambda '(:lambda (:x :y) (:+ :x :y)) *env*)
+      `(:closure (:x :y) (:+ :x :y) ,*env*)))
+
+(subtest "eval-list"
+  (is (eval-list '(1 2 3 4) *env*)
+      '(1 2 3 4)))
+
+(subtest "_eval"
+  (is (_eval '(:+ :x :y) #H(:x 2 :y 4 :z 4 :+ '(:prim +))) 6))
 
 (subtest "si/eval"
   (is (si/eval "3") 3)
   (is (si/eval "3.2") 3.2)
   (is (si/eval "100/20") 5)
-  (is (si/eval "(:+ 1 2)") 3))
+  (is (si/eval "(:+ 1 2)") 3)
+  (is (si/eval "((:lambda (:a :b) (:+ :a :b)) 3 2)") 5)
+  (is (si/eval "((:lambda (:x :y :z) (:+ :x :y :z)) 3 2 5)") 10)
+  (is (si/eval "(:let ((:z 3))
+                  ((:lambda (:x :y) (:+ :x :y :z))
+                   2 5))")
+      10)
+  (is (si/eval "(:let ((:x 3))
+                  (:let ((:fun (:lambda (:y) (:+ :x :y))))
+                    (:+ (:fun 1) (:fun 2))))")
+      9))
 
 (finalize)
